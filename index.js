e4d9c6d2f0b9cd64d91f025a2722eb86cefbf5b8
jpaul17251 <paimacaa@outlook.es>
2019-11-16 15:47:36 -0500

    Version 1

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200).send({
+      message:'SERVICIO GRUPO 5'
+  })
+});
+app.listen(3000, function () {
+  console.log('Servidor en el puerto 3000');
+});
