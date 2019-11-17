6b0e5ccf53ce2271be18e139e6f9e06e1a6b8421
AlexanderCR8 <56895019+AlexanderCR8@users.noreply.github.com>
2019-11-16 21:02:23 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
+'use strict'
+
 const express = require('express');
 const app = express();
+const request = require('request');
+const async = require( 'async');
 const PORT = process.env.PORT || 3000
 
+
 app.get('/', (req, res) => {
-  res.status(200).send({
-      message:'SERVICIO GRUPO 1'
-  })
+    res.json({
+
+   "mensaje": [
+        {
+                "msg": "Servicio grupo 1"
+        }
+   ]
+
 });
+});
+
 app.listen(PORT, function () {
   console.log('Servidor en el puerto 3000');
 });
