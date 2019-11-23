5a30410fa9028c2bc603225792828b734a764e76
AlexanderCR8 <56895019+AlexanderCR8@users.noreply.github.com>
2019-11-23 12:47:11 -0500

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ app.get('/', (req, res) => {
    "mensaje": [
         {
                 "msg": "Servicio grupo 1"
+                "msg2": "El Estudiante se ha editado correctamente"
+                "msg3": "El Estudiante se ha eliminado correctamente"
+                "msg4": "Se produjo un problema en la Edición/Eliminación"
         }
    ]
 
